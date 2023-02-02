55d12752fd668738a7e2447b0b7fbf44c2cc4d59
Shubham15Bit <shubham@beimagine.tech>
2023-02-02 11:54:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Header from "./Header";
+import Checkout from "./Checkout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <Router>
+        <Routes>
+          <Route path="/checkout" element={<><Header/><Checkout /></>} />
+          <Route path="/" element={<><Header/><Home /></>} />
+        </Routes>
+      </Router>
     </div>
+
+    /* ---->can be implemented like below as well */
+    //    <div className="app">
+    //    <Router>
+    //      <Routes>
+    //        <Route
+    //          path="/checkout"
+    //          element={
+    //            <>
+    //              <Header />
+    //              <Checkout/>
+    //            </>
+    //          }
+    //        />
+    //        <Route
+    //          path="/"
+    //          element={
+    //            <>
+    //              <Header />
+    //              <Home />
+    //            </>
+    //          }
+    //        />
+    //      </Routes>
+    //    </Router>
+    //  </div>
   );
 }
-
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDIQ5NAZH3_fvk5-I2mE54LPRMeryUog58",
+  authDomain: "clone-8c3e1.firebaseapp.com",
+  projectId: "clone-8c3e1",
+  storageBucket: "clone-8c3e1.appspot.com",
+  messagingSenderId: "150243558455",
+  appId: "1:150243558455:web:75bdf1dc29e3b3132debb2",
+  measurementId: "G-LW3TMDYEMY",
+};
