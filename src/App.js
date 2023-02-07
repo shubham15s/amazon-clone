2d2dd023eeb4f5b3dfe4b402bac0ab09197832dd
Shubham Jaiswal <112847786+Shubham15Bit@users.noreply.github.com>
2023-02-07 21:31:11 +0530

    login page and auth integratio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
 import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useStateValue } from "./StateProvider";
+
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    //run once when the app components loads
+    onAuthStateChanged(auth, (authUser) => {
+      console.log("The user is ", authUser);
+      if(authUser) {
+        //the user just logged in
+        dispatch({
+          type:'SET_USER',
+          user: authUser
+        })
+      }
+      else {
+        // the user is logged out
+        dispatch({
+          type:'SET_USER',
+          user:null
+        })
+      }
+    });
+  }, []);
   return (
     <div className="app">
-      
       <Router>
         <Routes>
-          <Route path="/checkout" element={<><Header/><Checkout /></>} />
-          <Route path="/" element={<><Header/><Home /></>} />
+          <Route
+            path="/login"
+            element={
+              <>
+                <Login />
+              </>
+            }
+          />
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDIQ5NAZH3_fvk5-I2mE54LPRMeryUog58",
   authDomain: "clone-8c3e1.firebaseapp.com",
@@ -8,3 +12,10 @@ const firebaseConfig = {
   appId: "1:150243558455:web:75bdf1dc29e3b3132debb2",
   measurementId: "G-LW3TMDYEMY",
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+const auth = getAuth();
+
+export { db, auth };
